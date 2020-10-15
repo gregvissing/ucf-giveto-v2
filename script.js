1ed9dc485b345bd7cc4f835ded91449a80ed6b58
Greg Vissing <greg.vissing@gmail.com>
2020-10-15 17:17:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -56,10 +56,72 @@ function initMiniDonationFormValues() {
 		.addClass("active");
 }
 
+function updateUrl(url, key, value) {
+	if (value !== undefined) {
+		value = encodeURI(value);
+	}
+	var hashIndex = url.indexOf("#") | 0;
+	if (hashIndex === -1) hashIndex = url.length | 0;
+	var urls = url.substring(0, hashIndex).split("?");
+	var baseUrl = urls[0];
+	var parameters = "";
+	var outPara = {};
+	if (urls.length > 1) {
+		parameters = urls[1];
+	}
+	if (parameters !== "") {
+		parameters = parameters.split("&");
+		for (k in parameters) {
+			var keyVal = parameters[k];
+			keyVal = keyVal.split("=");
+			var ekey = keyVal[0];
+			var evalue = "";
+			if (keyVal.length > 1) {
+				evalue = keyVal[1];
+			}
+			outPara[ekey] = evalue;
+		}
+	}
+
+	if (value !== undefined) {
+		outPara[key] = value;
+	} else {
+		delete outPara[key];
+	}
+	parameters = [];
+	for (var k in outPara) {
+		parameters.push(k + "=" + outPara[k]);
+	}
+
+	var finalUrl = baseUrl;
+
+	if (parameters.length > 0) {
+		finalUrl += "?" + parameters.join("&");
+	}
+
+	return finalUrl + url.substring(hashIndex);
+}
+
+function setURLparams() {
+	var donateUrl = $("a.donate").attr("href");
+	var amountUrl = $(".dollar-box.active input").data("value");
+	var typeUrl = $(".gift-box.active input").data("value");
+	
+	console.log(amountUrl);
+	var myURL = donateUrl;
+
+	myURL = updateUrl(myURL, "amount", amountUrl);
+	myURL = updateUrl(myURL, "type", typeUrl);
+	console.log("added location..." + myURL);
+	
+	$("a.donate").attr("href", myURL);
+}
+
 setAmounts();
 setGiftTypes();
 
 initMiniDonationFormValues();
+setURLparams();
 
 $(function () {
 	$("input.SearchTextBox").attr("placeholder", "Search");
